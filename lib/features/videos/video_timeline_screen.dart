22cd1a5e9dff874cfeebda99bb48feeb1d45c80b
hsimao <e087754958@gmail.com>
2024-01-09 10:49:50 +0800

    feat: infinite timeline screent

---
+++ lib/features/videos/video_timeline_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class VideoTimelineScreen extends StatefulWidget {
+  const VideoTimelineScreen({super.key});
+
+  @override
+  State<VideoTimelineScreen> createState() => _VideoTimelineScreenState();
+}
+
+class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
+  List<Color> colors = [
+    Colors.blue,
+    Colors.red,
+    Colors.yellow,
+    Colors.teal,
+  ];
+
+  int _itemCount = 4;
+
+  void _onPageChanged(int page) {
+    if (page == _itemCount - 1) {
+      _itemCount = _itemCount + 4;
+      colors.addAll([
+        Colors.blue,
+        Colors.red,
+        Colors.yellow,
+        Colors.teal,
+      ]);
+      setState(() {});
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      onPageChanged: _onPageChanged,
+      scrollDirection: Axis.vertical,
+      itemCount: _itemCount,
+      itemBuilder: (context, index) => Container(
+        color: colors[index],
+        child: Center(
+          child: Text(
+            "Screen $index",
+            style: const TextStyle(fontSize: 30),
+          ),
+        ),
+      ),
+    );
+  }
+}
