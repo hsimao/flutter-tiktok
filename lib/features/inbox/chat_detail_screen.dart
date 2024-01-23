30d0a14123e505c580420a7aad36122461ba115b
hsimao <e087754958@gmail.com>
2024-01-23 21:15:43 +0800

    feat: chat detail screen item style

--- lib/features/inbox/chat_detail_screen.dart
+++ lib/features/inbox/chat_detail_screen.dart
@@ -1,10 +1,112 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 class ChatDetailScreen extends StatelessWidget {
   const ChatDetailScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const ListTile(
+          contentPadding: EdgeInsets.zero,
+          horizontalTitleGap: Sizes.size8,
+          leading: CircleAvatar(
+            radius: Sizes.size24,
+            foregroundImage: NetworkImage(
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCHbFQ3EEJmClH2-0xwgSsTK0Zi79XIAC4Pg&usqp=CAU"),
+            child: Text('MK'),
+          ),
+          title: Text(
+            'Hey',
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          subtitle: Text('Active now'),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              FaIcon(
+                FontAwesomeIcons.flag,
+                color: Colors.black,
+                size: Sizes.size20,
+              ),
+              Gaps.h32,
+              FaIcon(
+                FontAwesomeIcons.ellipsis,
+                color: Colors.black,
+                size: Sizes.size20,
+              )
+            ],
+          ),
+        ),
+      ),
+      body: Stack(
+        children: [
+          // 對話框
+          ListView.separated(
+            padding: const EdgeInsets.symmetric(
+              vertical: Sizes.size20,
+              horizontal: Sizes.size14,
+            ),
+            itemBuilder: (context, index) {
+              final isMine = index % 2 == 0;
+              return Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment:
+                    isMine ? MainAxisAlignment.end : MainAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(Sizes.size14),
+                    decoration: BoxDecoration(
+                      color:
+                          isMine ? Colors.blue : Theme.of(context).primaryColor,
+                      // 對話框圓角
+                      borderRadius: BorderRadius.only(
+                        topLeft: const Radius.circular(Sizes.size20),
+                        topRight: const Radius.circular(Sizes.size20),
+                        bottomLeft: Radius.circular(
+                            isMine ? Sizes.size20 : Sizes.size5),
+                        bottomRight: Radius.circular(
+                            !isMine ? Sizes.size20 : Sizes.size5),
+                      ),
+                    ),
+                    child: const Text(
+                      "this is a message!",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            },
+            separatorBuilder: (context, index) => Gaps.v10,
+            itemCount: 10,
+          ),
+          // 底部對話框
+          Positioned(
+            bottom: 0,
+            // 滿版寬度
+            width: MediaQuery.of(context).size.width,
+            child: BottomAppBar(
+              color: Colors.grey.shade50,
+              child: Row(
+                children: [
+                  const Expanded(child: TextField()),
+                  Gaps.h20,
+                  Container(
+                    child: const FaIcon(FontAwesomeIcons.paperPlane),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
