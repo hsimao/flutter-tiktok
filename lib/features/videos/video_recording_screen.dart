eda1da2f9fafcbceae692bdf185cf6460c21a817
hsimao <e087754958@gmail.com>
2024-02-01 18:04:05 +0800

    feat: video recording button animation

--- lib/features/videos/video_recording_screen.dart
+++ lib/features/videos/video_recording_screen.dart
@@ -12,12 +12,30 @@ class VideoRecordingScreen extends StatefulWidget {
   State<VideoRecordingScreen> createState() => _VideoRecordingScreenState();
 }
 
-class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
+class _VideoRecordingScreenState extends State<VideoRecordingScreen>
+    with TickerProviderStateMixin {
   bool _hasPermission = false;
 
   bool _isSelfieMode = false;
 
   late FlashMode _flashMode;
+
+  late final AnimationController _buttonAnimationController =
+      AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 200),
+  );
+
+  late final Animation<double> _buttonAnimation =
+      Tween(begin: 1.0, end: 1.3).animate(_buttonAnimationController);
+
+  late final AnimationController _progressAnimationController =
+      AnimationController(
+    vsync: this,
+    duration: const Duration(seconds: 10),
+    lowerBound: 0.0,
+    upperBound: 1.0,
+  );
 
   late CameraController _cameraController;
 
@@ -67,49 +85,106 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
     setState(() {});
   }
 
+  void _startRecording(TapDownDetails _) {
+    print("START RECORDING");
+    _buttonAnimationController.forward();
+    _progressAnimationController.forward();
+  }
+
+  void _stopRecording() {
+    print("STOP RECORDING");
+    _buttonAnimationController.reverse();
+    _progressAnimationController.reset();
+  }
+
   @override
   initState() {
     super.initState();
     initPermissions();
+
+    // 只要 CircularProgress 有變化就會觸發 setState
+    _progressAnimationController.addListener(() {
+      setState(() {});
+    });
+
+    // CircularProgress 動畫結束後觸發
+    _progressAnimationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _stopRecording();
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.black,
-        body: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          child: !_hasPermission || !_cameraController.value.isInitialized
-              ? const Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Initializing...",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: Sizes.size20,
+      backgroundColor: Colors.black,
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: !_hasPermission || !_cameraController.value.isInitialized
+            ? const Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Initializing...",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: Sizes.size20,
+                    ),
+                  ),
+                  Gaps.v20,
+                  CircularProgressIndicator.adaptive(),
+                ],
+              )
+            : Stack(
+                alignment: Alignment.center,
+                children: [
+                  CameraPreview(_cameraController),
+                  Positioned(
+                    top: Sizes.size56,
+                    right: Sizes.size20,
+                    child: CameraControlButtons(
+                      flashMode: _flashMode,
+                      setFlashMode: _setFlashMode,
+                      toggleSelfieMode: _toggleSelfieMode,
+                    ),
+                  ),
+                  Positioned(
+                    bottom: Sizes.size40,
+                    child: GestureDetector(
+                      onTapDown: _startRecording,
+                      onTapUp: (details) => _stopRecording(),
+                      child: ScaleTransition(
+                        scale: _buttonAnimation,
+                        child: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            SizedBox(
+                              width: Sizes.size80 + Sizes.size14,
+                              height: Sizes.size80 + Sizes.size14,
+                              child: CircularProgressIndicator(
+                                color: Colors.red.shade400,
+                                strokeWidth: Sizes.size6,
+                                value: _progressAnimationController.value,
+                              ),
+                            ),
+                            Container(
+                              width: Sizes.size80,
+                              height: Sizes.size80,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.red.shade500,
+                              ),
+                            )
+                          ],
+                        ),
                       ),
                     ),
-                    Gaps.v20,
-                    CircularProgressIndicator.adaptive(),
-                  ],
-                )
-              : Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    CameraPreview(_cameraController),
-                    Positioned(
-                      top: Sizes.size56,
-                      right: Sizes.size20,
-                      child: CameraControlButtons(
-                        flashMode: _flashMode,
-                        setFlashMode: _setFlashMode,
-                        toggleSelfieMode: _toggleSelfieMode,
-                      ),
-                    )
-                  ],
-                ),
-        ));
+                  )
+                ],
+              ),
+      ),
+    );
   }
 }
