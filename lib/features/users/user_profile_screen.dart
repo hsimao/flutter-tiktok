6353f7fedd73bcd31401a1cc639d3131eed8106c
hsimao <e087754958@gmail.com>
2024-02-15 11:22:23 +0800

    feat: user profile screen change to consumer widget

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/settings/settings_screen.dart';
+import 'package:tiktok_clone/features/users/view_models/users_view_model.dart';
 import 'package:tiktok_clone/features/users/widgets/persistent_tab_bar.dart';
 
-class UserProfileScreen extends StatefulWidget {
+class UserProfileScreen extends ConsumerStatefulWidget {
   final String username;
   final String tab;
 
@@ -16,10 +18,10 @@ class UserProfileScreen extends StatefulWidget {
   });
 
   @override
-  State<UserProfileScreen> createState() => _UserProfileScreenState();
+  ConsumerState<UserProfileScreen> createState() => _UserProfileScreenState();
 }
 
-class _UserProfileScreenState extends State<UserProfileScreen> {
+class _UserProfileScreenState extends ConsumerState<UserProfileScreen> {
   void _onGearPressed() {
     Navigator.of(context).push(
       MaterialPageRoute(
@@ -30,194 +32,203 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
-      body: SafeArea(
-        child: DefaultTabController(
-          initialIndex: widget.tab == "likes" ? 1 : 0,
-          length: 2,
-          child: NestedScrollView(
-            headerSliverBuilder: (context, innerBoxIsScrolled) {
-              return [
-                SliverAppBar(
-                  title: Text(widget.username),
-                  actions: [
-                    IconButton(
-                      onPressed: _onGearPressed,
-                      icon: const FaIcon(
-                        FontAwesomeIcons.gear,
-                        size: Sizes.size20,
-                      ),
-                    ),
-                  ],
-                ),
-                SliverToBoxAdapter(
-                  child: Column(
-                    children: [
-                      Gaps.v20,
-                      const CircleAvatar(
-                        radius: 50,
-                        foregroundImage: NetworkImage(
-                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgH_It5rNS9RokW8mTTyZKzpoG8ApY4_YOvw&usqp=CAU"),
-                        child: Text("Mark"),
-                      ),
-                      Gaps.v20,
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "@${widget.username}",
-                            style: const TextStyle(
-                              fontWeight: FontWeight.w600,
-                              fontSize: Sizes.size18,
+    return ref.watch(usersProvider).when(
+          error: (error, stackTrace) => Center(
+            child: Text(error.toString()),
+          ),
+          loading: () => const Center(
+            child: CircularProgressIndicator.adaptive(),
+          ),
+          data: (data) => Scaffold(
+            backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
+            body: SafeArea(
+              child: DefaultTabController(
+                initialIndex: widget.tab == "likes" ? 1 : 0,
+                length: 2,
+                child: NestedScrollView(
+                  headerSliverBuilder: (context, innerBoxIsScrolled) {
+                    return [
+                      SliverAppBar(
+                        title: Text(widget.username),
+                        actions: [
+                          IconButton(
+                            onPressed: _onGearPressed,
+                            icon: const FaIcon(
+                              FontAwesomeIcons.gear,
+                              size: Sizes.size20,
                             ),
                           ),
-                          Gaps.h5,
-                          FaIcon(
-                            FontAwesomeIcons.solidCircleCheck,
-                            size: Sizes.size16,
-                            color: Colors.blue.shade500,
-                          ),
-                          Gaps.v5,
                         ],
                       ),
-                      Gaps.v24,
-
-                      // 追蹤、被追蹤、Likes 數量
-                      SizedBox(
-                        height: Sizes.size48,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
+                      SliverToBoxAdapter(
+                        child: Column(
                           children: [
-                            const UserProfileStatisticItem(
-                              primary: '97',
-                              secondary: 'Following',
+                            Gaps.v20,
+                            const CircleAvatar(
+                              radius: 50,
+                              foregroundImage: NetworkImage(
+                                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgH_It5rNS9RokW8mTTyZKzpoG8ApY4_YOvw&usqp=CAU"),
+                              child: Text("Mark"),
                             ),
-                            VerticalDivider(
-                              width: Sizes.size32,
-                              thickness: Sizes.size1,
-                              color: Colors.grey.shade400,
-                              indent: Sizes.size14,
-                              endIndent: Sizes.size14,
+                            Gaps.v20,
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "@${widget.username}",
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: Sizes.size18,
+                                  ),
+                                ),
+                                Gaps.h5,
+                                FaIcon(
+                                  FontAwesomeIcons.solidCircleCheck,
+                                  size: Sizes.size16,
+                                  color: Colors.blue.shade500,
+                                ),
+                                Gaps.v5,
+                              ],
                             ),
-                            const UserProfileStatisticItem(
-                              primary: '10M',
-                              secondary: 'Followers',
+                            Gaps.v24,
+
+                            // 追蹤、被追蹤、Likes 數量
+                            SizedBox(
+                              height: Sizes.size48,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const UserProfileStatisticItem(
+                                    primary: '97',
+                                    secondary: 'Following',
+                                  ),
+                                  VerticalDivider(
+                                    width: Sizes.size32,
+                                    thickness: Sizes.size1,
+                                    color: Colors.grey.shade400,
+                                    indent: Sizes.size14,
+                                    endIndent: Sizes.size14,
+                                  ),
+                                  const UserProfileStatisticItem(
+                                    primary: '10M',
+                                    secondary: 'Followers',
+                                  ),
+                                  VerticalDivider(
+                                    width: Sizes.size32,
+                                    thickness: Sizes.size1,
+                                    color: Colors.grey.shade400,
+                                    indent: Sizes.size14,
+                                    endIndent: Sizes.size14,
+                                  ),
+                                  const UserProfileStatisticItem(
+                                    primary: '194.3M',
+                                    secondary: 'Likes',
+                                  ),
+                                ],
+                              ),
                             ),
-                            VerticalDivider(
-                              width: Sizes.size32,
-                              thickness: Sizes.size1,
-                              color: Colors.grey.shade400,
-                              indent: Sizes.size14,
-                              endIndent: Sizes.size14,
+                            Gaps.v14,
+
+                            // Follow 按鈕
+                            FractionallySizedBox(
+                              widthFactor: 0.33,
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: Sizes.size12,
+                                ),
+                                decoration: BoxDecoration(
+                                  color: Theme.of(context).primaryColor,
+                                  borderRadius: const BorderRadius.all(
+                                    Radius.circular(Sizes.size4),
+                                  ),
+                                ),
+                                child: const Text(
+                                  "Follow",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
                             ),
-                            const UserProfileStatisticItem(
-                              primary: '194.3M',
-                              secondary: 'Likes',
+                            Gaps.v14,
+                            const Padding(
+                              padding: EdgeInsets.symmetric(
+                                horizontal: Sizes.size32,
+                              ),
+                              child: Text(
+                                "All highlights and where to watch live matches on FIFA+ I wonder how it would loook",
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                            Gaps.v14,
+
+                            // Links
+                            const Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                FaIcon(
+                                  FontAwesomeIcons.link,
+                                  size: Sizes.size12,
+                                ),
+                                Gaps.h4,
+                                Text(
+                                  'https://google.com',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                )
+                              ],
+                            ),
+                            Gaps.v20,
+                          ],
+                        ),
+                      ), // Tabs
+                      SliverPersistentHeader(
+                        delegate: PersistentTabBar(),
+                        pinned: true,
+                      ),
+                    ];
+                  },
+                  body: TabBarView(
+                    children: [
+                      GridView.builder(
+                        // 限制不可滾動
+                        physics: const NeverScrollableScrollPhysics(),
+                        // 當拖曳時，讓鍵盤自動收起
+                        keyboardDismissBehavior:
+                            ScrollViewKeyboardDismissBehavior.onDrag,
+                        itemCount: 20,
+                        padding: EdgeInsets.zero,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          crossAxisSpacing: Sizes.size2,
+                          mainAxisSpacing: Sizes.size2,
+                          childAspectRatio: 9 / 14,
+                        ),
+                        itemBuilder: (context, index) => Column(
+                          children: [
+                            AspectRatio(
+                              aspectRatio: 9 / 14,
+                              child: FadeInImage.assetNetwork(
+                                  fit: BoxFit.cover,
+                                  placeholder: 'assets/images/placeholder.jpg',
+                                  image:
+                                      'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/b7/b3/eb/caption.jpg?w=1200&h=-1&s=1'),
                             ),
                           ],
                         ),
                       ),
-                      Gaps.v14,
-
-                      // Follow 按鈕
-                      FractionallySizedBox(
-                        widthFactor: 0.33,
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(
-                            vertical: Sizes.size12,
-                          ),
-                          decoration: BoxDecoration(
-                            color: Theme.of(context).primaryColor,
-                            borderRadius: const BorderRadius.all(
-                              Radius.circular(Sizes.size4),
-                            ),
-                          ),
-                          child: const Text(
-                            "Follow",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w600,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ),
-                      Gaps.v14,
-                      const Padding(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: Sizes.size32,
-                        ),
-                        child: Text(
-                          "All highlights and where to watch live matches on FIFA+ I wonder how it would loook",
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                      Gaps.v14,
-
-                      // Links
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          FaIcon(
-                            FontAwesomeIcons.link,
-                            size: Sizes.size12,
-                          ),
-                          Gaps.h4,
-                          Text(
-                            'https://google.com',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                            ),
-                          )
-                        ],
-                      ),
-                      Gaps.v20,
-                    ],
-                  ),
-                ), // Tabs
-                SliverPersistentHeader(
-                  delegate: PersistentTabBar(),
-                  pinned: true,
-                ),
-              ];
-            },
-            body: TabBarView(
-              children: [
-                GridView.builder(
-                  // 限制不可滾動
-                  physics: const NeverScrollableScrollPhysics(),
-                  // 當拖曳時，讓鍵盤自動收起
-                  keyboardDismissBehavior:
-                      ScrollViewKeyboardDismissBehavior.onDrag,
-                  itemCount: 20,
-                  padding: EdgeInsets.zero,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                    crossAxisSpacing: Sizes.size2,
-                    mainAxisSpacing: Sizes.size2,
-                    childAspectRatio: 9 / 14,
-                  ),
-                  itemBuilder: (context, index) => Column(
-                    children: [
-                      AspectRatio(
-                        aspectRatio: 9 / 14,
-                        child: FadeInImage.assetNetwork(
-                            fit: BoxFit.cover,
-                            placeholder: 'assets/images/placeholder.jpg',
-                            image:
-                                'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/b7/b3/eb/caption.jpg?w=1200&h=-1&s=1'),
-                      ),
+                      const Center(child: Text('page two')),
                     ],
                   ),
                 ),
-                const Center(child: Text('page two')),
-              ],
+              ),
             ),
           ),
-        ),
-      ),
-    );
+        );
   }
 }
 
